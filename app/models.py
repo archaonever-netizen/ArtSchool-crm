f4e028a7a5bc3df3d282aace5c888c11c01d7e39
archaonever-netizen <archaonever@gmail.com>
2026-05-17 19:21:27 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from datetime import datetime
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), unique=True, nullable=False)  # для ученика = телефон
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(256), nullable=False)
     role = db.Column(db.String(20), nullable=False)  # admin, manager, teacher, student
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
